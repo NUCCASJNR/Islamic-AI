7f7c9e8ed1481e1cefdfaf7e2626ff53e937dfab
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-02 23:09:33 +0100

    Project about ot get started in fullest

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 -- sql script
 
 
-CREATE DATABASE IF NOT EXISTS rentease_db;
-       CREATE USER IF NOT EXISTS 'rent_user'@'localhost' IDENTIFIED BY 'Rent_pwd123@';
-              GRANT ALL PRIVILEGES ON rentease_db.* TO 'rent_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS chatbot_db;
+       CREATE USER IF NOT EXISTS 'chat'@'localhost' IDENTIFIED BY 'Chat_pwd123@';
+              GRANT ALL PRIVILEGES ON chatbot_db.* TO 'chat'@'localhost';
                                       GRANT SELECT ON performance_schema.* TO 'rent_user'@'localhost';
 FLUSH PRIVILEGES;
