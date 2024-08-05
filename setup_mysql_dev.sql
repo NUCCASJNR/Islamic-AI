90306dd2d79e2412c84f6db0de7cf9135c81f041
Restyled.io <commits@restyled.io>
2024-08-05 15:36:09 +0000

    Restyled by pg_format

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,12 @@
 -- sql script
-
-
 CREATE DATABASE IF NOT EXISTS chatbot_db;
-       CREATE USER IF NOT EXISTS 'chat'@'localhost' IDENTIFIED BY 'Chat_pwd123@';
-              GRANT ALL PRIVILEGES ON chatbot_db.* TO 'chat'@'localhost';
-                                      GRANT SELECT ON performance_schema.* TO 'rent_user'@'localhost';
+
+CREATE USER IF NOT EXISTS 'chat' @'localhost' IDENTIFIED BY
+    'Chat_pwd123@';
+
+GRANT ALL PRIVILEGES ON chatbot_db.* TO 'chat' @'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'rent_user' @'localhost';
+
 FLUSH PRIVILEGES;
+
