700d5bcdc9cb3a0cead1b2f0eb8cd91a5a5816ac
Restyled.io <commits@restyled.io>
2024-08-06 13:23:11 +0000

    Restyled by whitespace

--- users/api.py
+++ users/api.py
@@ -35,7 +35,7 @@ def signup(request, payload: UserCreateSchema):
     :param payload: User payload
     :param payload: UserCreateSchema:
     :param payload: UserCreateSchema:
-    :param payload: UserCreateSchema: 
+    :param payload: UserCreateSchema:
     :returns: 201 or 400
 
     """
