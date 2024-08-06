303c57db1e490ffe9831e8f529472d954eceb49f
Restyled.io <commits@restyled.io>
2024-08-06 21:16:10 +0000

    Restyled by pyment

--- users/models.py
+++ users/models.py
@@ -23,7 +23,9 @@ def hash_password(password: Union[str, int]) -> str:
     :param password: Union[str:
     :param int: returns: The hashed password
     :param password: Union[str:
-    :param int]:
+    :param int: returns: The hashed password
+    :param password: Union[str: 
+    :param int]: 
     :returns: The hashed password
 
     """
@@ -62,7 +64,7 @@ class MainUser(AbstractUser, BaseModel):
     def custom_save(cls, **kwargs):
         """Overrides the custom_save method to hash the password before saving
 
-        :param **kwargs:
+        :param **kwargs: 
 
         """
         if "password" in kwargs:
@@ -96,6 +98,8 @@ class MainUser(AbstractUser, BaseModel):
         :param last_name: str:
         :param first_name: str:
         :param last_name: str:
+        :param first_name: str: 
+        :param last_name: str: 
 
         """
         self.first_name = first_name
