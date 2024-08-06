e11b1b571a459926468d9a8d3e6656789142621b
Restyled.io <commits@restyled.io>
2024-08-06 21:16:11 +0000

    Restyled by whitespace

--- users/models.py
+++ users/models.py
@@ -27,8 +27,8 @@ def hash_password(password: Union[str, int]) -> str:
     :param int: returns: The hashed password
     :param password: Union[str:
     :param int: returns: The hashed password
-    :param password: Union[str: 
-    :param int]: 
+    :param password: Union[str:
+    :param int]:
     :returns: The hashed password
 
     """
@@ -67,7 +67,7 @@ class MainUser(AbstractUser, BaseModel):
     def custom_save(cls, **kwargs):
         """Overrides the custom_save method to hash the password before saving
 
-        :param **kwargs: 
+        :param **kwargs:
 
         """
         if "password" in kwargs:
@@ -101,8 +101,8 @@ class MainUser(AbstractUser, BaseModel):
         :param last_name: str:
         :param first_name: str:
         :param last_name: str:
-        :param first_name: str: 
-        :param last_name: str: 
+        :param first_name: str:
+        :param last_name: str:
 
         """
         self.first_name = first_name
