b6541a12f7686cf40cc7bb8469afcef072e58b65
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-09 05:11:23 +0100

    Created messages model

--- chat/models.py
+++ chat/models.py
@@ -11,6 +11,17 @@ CONVERSATION_CHOICES = [
     ('escalated', 'Escalated')
 ]
 
+MESSAGE_CHOICES = [
+    ('text', 'Text'),
+    ('suggestion', 'Suggestion'),
+    ('follow-up', 'Follow-up')
+]
+
+SENDER_CHOICES = [
+    ('user', 'User'),
+    ('bot', 'Bot')
+]
+
 
 class Conversation(BaseModel):
     """Conversation Model"""
@@ -24,3 +35,17 @@ class Conversation(BaseModel):
 
     def __str__(self):
         return f"Conversation {self.id} with {self.user.username}"
+
+
+class Message(models.Model):
+    """Message Model"""
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.CharField(max_length=10, choices=SENDER_CHOICES)
+    message_text = models.TextField()
+    response_type = models.CharField(max_length=50, choices=MESSAGE_CHOICES)
+
+    class Meta:
+        db_table = "messages"
+
+    def __str__(self):
+        return f"Message {self.id} from {self.sender} in {self.conversation.id}"
