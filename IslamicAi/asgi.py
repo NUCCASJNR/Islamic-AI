760d64df665daf1f5181ae4577ac10bc9e3be5d9
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-18 22:50:27 +0100

    Fixed pytest error

--- IslamicAi/asgi.py
+++ IslamicAi/asgi.py
@@ -1,29 +1,27 @@
 """
 ASGI config for IslamicAi project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named `application.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-import django
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IslamicAi.settings")
-django.setup()  # Ensures that Django is fully set up before importing other components
+django_asgi_app = get_asgi_application()
 
-# Import websocket_urlpatterns after setting up Django
 from chat.routing import websocket_urlpatterns
 
-# Define the ASGI application
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
