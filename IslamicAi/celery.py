d1be74a1e36b4a0a0bce0665e9b2b771651c6f8b
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-18 23:40:10 +0100

    update celery.py

--- IslamicAi/celery.py
+++ IslamicAi/celery.py
@@ -16,7 +16,7 @@ app = Celery('IslamicAi')
 app.conf.beat_schedule = {
     'send-daily-hadith': {
         'task': 'utils.tasks.send_hadith',
-        'schedule': crontab(hour=10, minute=43),
+        'schedule': crontab(hour=11, minute=45),
     },
 }
 app.config_from_object('django.conf:settings', namespace='CELERY')
