afe1c490d4cca5a2a56a2e2ecf21bc6516e966ea
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-18 16:08:43 +0100

    Fixed pycodestyle error

--- IslamicAi/asgi.py
+++ IslamicAi/asgi.py
@@ -8,20 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IslamicAi.settings")
-django_asgi_app = get_asgi_application()
+django.setup()  # Ensures that Django is fully set up before importing other components
 
+# Import websocket_urlpatterns after setting up Django
 from chat.routing import websocket_urlpatterns
 
+# Define the ASGI application
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
