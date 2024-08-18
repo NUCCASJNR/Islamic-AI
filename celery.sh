b15e26addf22a216f314ccebbd70466f01bfa1e5
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-08-18 23:21:27 +0100

    update celery.sh

--- celery.sh
+++ celery.sh
@@ -1,1 +1,7 @@
-python3 manage.py celery_worker
+#!/bin/bash
+
+# Start the Celery worker in the background
+python3 manage.py celery_worker &
+
+# Start the Django development server
+python3 manage.py runserver
